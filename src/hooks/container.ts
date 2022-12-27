b008e528e5a260f641aeb270e21fc13f2a324044
David Borzek <mail@davidborzek.de>
2022-12-27 14:53:15 +0100

    feat: add pull to refresh

--- src/hooks/container.ts
+++ src/hooks/container.ts
@@ -2,15 +2,25 @@ import { useEffect, useState } from 'react';
 import { Container, fetchContainers } from '../supabase/supabase';
 
 export function useContainers() {
+  const [refreshing, setRefreshing] = useState(false);
   let [containers, setContainers] = useState<Container[]>([]);
 
   const fetch = () => {
     fetchContainers().then(setContainers);
   };
 
+  const refresh = () => {
+    setRefreshing(true);
+    return fetchContainers()
+    .then(setContainers)
+    .finally(() => {
+      setRefreshing(false);
+    });
+  }
+
   useEffect(() => {
     fetch();
   }, []);
 
-  return { containers, fetch };
+  return { containers, fetch, refresh, refreshing };
 }
