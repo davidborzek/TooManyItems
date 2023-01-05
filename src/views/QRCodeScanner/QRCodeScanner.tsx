6d90aa78a314ab9b1ead976c0768d7adcbc6c02f
David Borzek <mail@davidborzek.de>
2023-01-05 16:07:25 +0100

    feat: scan container qr code

--- src/views/QRCodeScanner/QRCodeScanner.tsx
+++ src/views/QRCodeScanner/QRCodeScanner.tsx
@@ -1,41 +1,76 @@
 import { useEffect, useState } from 'react';
-import { BarCodeScanner, BarCodeScannedCallback } from 'expo-barcode-scanner';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AppStackParamList } from '../../App';
+import { fetchContainer } from '../../supabase/supabase';
+import React from 'react';
+import { BarCodeScanningResult, Camera } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
+import FullSpinner from '../../components/FullSpinner/FullSpinner';
 
 type Props = NativeStackScreenProps<AppStackParamList, 'QRCodeScanner'>;
 
 export default function QRCodeScanner({ navigation }: Props) {
+  const { t } = useTranslation();
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [scanned, setScanned] = useState(false);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
-    BarCodeScanner.requestPermissionsAsync().then(({ status }) => {
+    Camera.requestCameraPermissionsAsync().then(({ status }) => {
       setHasPermission(status === 'granted');
     });
   }, []);
 
-  const handleBarCodeScanned: BarCodeScannedCallback = ({ type, data }) => {
+  const showErrorAlert = () => {
+    Alert.alert(t('error'), t('invalid_qr_code') || '', [
+      {
+        onPress: () => setScanned(false),
+      },
+    ]);
+  };
+
+  const handleBarCodeScanned = ({ data }: BarCodeScanningResult) => {
+    if (scanned) {
+      return;
+    }
+
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    // TODO: open container detail view
+
+    const id = parseInt(data, 10);
+    if (!id) {
+      showErrorAlert();
+      return;
+    }
+
+    fetchContainer(id).then((container) => {
+      setScanned(false);
+      navigation.navigate('ContainerView', { container });
+    });
   };
 
   if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+    return <FullSpinner />;
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>{t('no_camera_permission')}</Text>;
   }
 
   return (
     <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={!scanned ? handleBarCodeScanned : undefined}
-        style={StyleSheet.absoluteFillObject}
-        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-      />
+      {isFocused ? (
+        <Camera
+          onBarCodeScanned={handleBarCodeScanned}
+          barCodeScannerSettings={{
+            barCodeTypes: ['qr'],
+          }}
+          style={{ flex: 1 }}
+          ratio="16:9"
+        />
+      ) : (
+        <></>
+      )}
     </View>
   );
 }
