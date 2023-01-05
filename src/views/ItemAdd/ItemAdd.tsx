6287994a3e53aaeabc3825a04c4f411927e3de10
David Borzek <mail@davidborzek.de>
2023-01-05 14:40:22 +0100

    feat: adapt add item view

--- src/views/ItemAdd/ItemAdd.tsx
+++ src/views/ItemAdd/ItemAdd.tsx
@@ -1,11 +1,19 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Image, TextInput, TouchableOpacity, View } from "react-native";
-import { FAB, Icon, Text } from "react-native-elements";
-import { AppStackParamList } from "../../App";
-import { useImagePicker } from "../../hooks/image";
-import { Container, insertItem } from "../../supabase/supabase";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  Image,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
+import { Text } from 'react-native-elements';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { AppStackParamList } from '../../App';
+import HeaderCheckmark from '../../components/HeaderCheckmark/HeaderCheckmark';
+import { useImagePicker } from '../../hooks/image';
+import { Container, insertItem } from '../../supabase/supabase';
 
 type Props = NativeStackScreenProps<AppStackParamList, 'ItemAdd'>;
 
@@ -20,79 +28,82 @@ export default function ItemAdd({ route, navigation }: Props) {
   const [description, setDescription] = useState('');
   const { container } = route.params;
 
+
   const handleCreateItem = async () => {
     await insertItem({
       container_id: container.id,
       name: itemName,
       description: description,
-      image: image
-    })
+      image: image,
+    });
 
     navigation.goBack();
   };
 
-  return (
-    <View style={{ alignItems: 'center', flex: 1 }}>
-      <TouchableOpacity
-        onPress={pickImage}
-        style={{
-          backgroundColor: '#c1c1c1',
-          marginVertical: 5,
-          minWidth: 300,
-          maxWidth: 300,
-          marginTop: 20,
-          height: 300,
-        }}
-      >
-        {image && (
-          <Image
-            source={{ uri: image }}
-            style={{ width: '100%', height: '100%' }}
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <HeaderCheckmark
+            disabled={!itemName}
+            onPress={handleCreateItem}
           />
-        )}
-      </TouchableOpacity>
-      <View style={{ alignItems: 'flex-start', width: 300, marginTop: 10 }}>
-        <Text>{t('name')}</Text>
-        <TextInput
-          style={{
-            height: 30,
-            marginVertical: 5,
-            borderWidth: 1,
-            padding: 10,
-            width: '100%',
-          }}
-          onChangeText={setItemName}
-          value={itemName}
-        />
+        );
+      },
+    });
+  }, [navigation, itemName]);
 
-        <Text>{t('description')}</Text>
-        <TextInput
+  return (
+    <KeyboardAwareScrollView>
+      <View style={styles.view}>
+        <TouchableOpacity
+          onPress={pickImage}
           style={{
-            height: 30,
+            backgroundColor: '#c1c1c1',
             marginVertical: 5,
-            borderWidth: 1,
-            padding: 10,
-            width: '100%',
+            minWidth: 300,
+            maxWidth: 300,
+            marginTop: 20,
+            height: 300,
           }}
-          onChangeText={setDescription}
-          value={description}
-        />
-      </View>
-      <FAB
-        title=""
-        color="#137b11"
-        placement="right"
-        icon={
-          <Icon
-            name="check"
-            size={24}
-            color="white"
-            tvParallaxProperties={undefined}
+        >
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: '100%', height: '100%' }}
+            />
+          )}
+        </TouchableOpacity>
+        <View style={styles.form}>
+          <Text>{t('name')}</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setItemName}
+            value={itemName}
           />
-        }
-        disabled={!itemName}
-        onPress={handleCreateItem}
-      />
-    </View>
-  )
+
+          <Text>{t('description')}</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setDescription}
+            value={description}
+          />
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  form: { marginTop: 10, width: '100%', paddingHorizontal: 20 },
+  input: {
+    marginVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+  },
+});
