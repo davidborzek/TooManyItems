6a42e041e7a4f90cf1512bf4c374cc471da38e02
David Borzek <mail@davidborzek.de>
2023-01-10 11:50:51 +0100

    fix: qr code button margin on all platforms

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -66,7 +66,7 @@ export default function Home({ navigation }: Props) {
           );
         },
         headerRightContainerStyle: {
-          marginRight: 16,
+          paddingRight: 16,
         },
       })}
     >
