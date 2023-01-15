86c02a9b73db9f38c14c7e8b50f5bd48244ecdcf
Nils Hartmann <nils1hartmann@gmail.com>
2023-01-15 14:47:37 +0100

    feat: added impressum

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -1,8 +1,8 @@
+import { Button, Text } from 'react-native-elements';
 import { StyleSheet, View } from 'react-native';
 
 import { AppStackParamList } from '../../App';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { Button } from 'react-native-elements';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { HomeTabParamList } from '../Home/Home';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -28,6 +28,14 @@ export default function Settings(_: Props) {
         onPress={() => supabase.auth.signOut()}
         buttonStyle={styles.signOut}
       />
+      <View style={{ marginTop: 50, marginBottom: 10, alignItems: 'center' }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 25 }}>Impressum</Text>
+      </View>
+      <Text>Verantwortlich für die App:</Text>
+      <Text>TooManyItems GmbH</Text>
+      <Text>Musterstraße 12</Text>
+      <Text>12345 Musterstadt</Text>
+      <Text>Deutschland</Text>
     </View>
   );
 }
